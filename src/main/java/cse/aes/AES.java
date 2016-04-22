8bf418b3d7c954e26ee27d3c42ed709b7bb78e1b
ShadowLordAlpha <shadow.alpha70@gmail.com>
2016-04-22 03:03:12 -0700

    decypher done

--- src/main/java/cse/aes/AES.java
+++ src/main/java/cse/aes/AES.java
@@ -8,26 +8,6 @@ public final class AES {
 	private static final int[] rcon = { 0x01000000, 0x02000000, 0x04000000, 0x08000000, 0x10000000, 0x20000000,
 			0x40000000, 0x80000000, 0x1b000000, 0x36000000 };
 
-	/*
-	 * Both the s box and inverse s box are hardcoded as char arrays though it
-	 * is possible to generate each array using an equation.
-	 */
-	private static final char[] invSBox = { 0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E,
-			0x81, 0xF3, 0xD7, 0xFB, 0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE,
-			0xE9, 0xCB, 0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E,
-			0x08, 0x2E, 0xA1, 0x66, 0x28, 0xD9, 0x24, 0xB2, 0x76, 0x5B, 0xA2, 0x49, 0x6D, 0x8B, 0xD1, 0x25, 0x72, 0xF8,
-			0xF6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xD4, 0xA4, 0x5C, 0xCC, 0x5D, 0x65, 0xB6, 0x92, 0x6C, 0x70, 0x48, 0x50,
-			0xFD, 0xED, 0xB9, 0xDA, 0x5E, 0x15, 0x46, 0x57, 0xA7, 0x8D, 0x9D, 0x84, 0x90, 0xD8, 0xAB, 0x00, 0x8C, 0xBC,
-			0xD3, 0x0A, 0xF7, 0xE4, 0x58, 0x05, 0xB8, 0xB3, 0x45, 0x06, 0xD0, 0x2C, 0x1E, 0x8F, 0xCA, 0x3F, 0x0F, 0x02,
-			0xC1, 0xAF, 0xBD, 0x03, 0x01, 0x13, 0x8A, 0x6B, 0x3A, 0x91, 0x11, 0x41, 0x4F, 0x67, 0xDC, 0xEA, 0x97, 0xF2,
-			0xCF, 0xCE, 0xF0, 0xB4, 0xE6, 0x73, 0x96, 0xAC, 0x74, 0x22, 0xE7, 0xAD, 0x35, 0x85, 0xE2, 0xF9, 0x37, 0xE8,
-			0x1C, 0x75, 0xDF, 0x6E, 0x47, 0xF1, 0x1A, 0x71, 0x1D, 0x29, 0xC5, 0x89, 0x6F, 0xB7, 0x62, 0x0E, 0xAA, 0x18,
-			0xBE, 0x1B, 0xFC, 0x56, 0x3E, 0x4B, 0xC6, 0xD2, 0x79, 0x20, 0x9A, 0xDB, 0xC0, 0xFE, 0x78, 0xCD, 0x5A, 0xF4,
-			0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7, 0x31, 0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F, 0x60, 0x51,
-			0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D, 0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF, 0xA0, 0xE0, 0x3B, 0x4D,
-			0xAE, 0x2A, 0xF5, 0xB0, 0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61, 0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77,
-			0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D };
-
 	public static final int AES128 = 1;
 	public static final int AES192 = 2;
 	public static final int AES256 = 3;
@@ -40,16 +20,17 @@ public final class AES {
 
 	// useful for debugging
 	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+
 	public static String bytesToHex(byte[] bytes) {
-	    char[] hexChars = new char[bytes.length * 2];
-	    for ( int j = 0; j < bytes.length; j++ ) {
-	        int v = bytes[j] & 0xFF;
-	        hexChars[j * 2] = hexArray[v >>> 4];
-	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-	    }
-	    return new String(hexChars);
+		char[] hexChars = new char[bytes.length * 2];
+		for (int j = 0; j < bytes.length; j++) {
+			int v = bytes[j] & 0xFF;
+			hexChars[j * 2] = hexArray[v >>> 4];
+			hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+		}
+		return new String(hexChars);
 	}
-	
+
 	public static final byte[] encrypt(byte[] key, byte[] data) {
 		/*
 		 * We do not allow for the key or the data to be null
@@ -70,7 +51,7 @@ public final class AES {
 		return Cypher.encrypt(key, data);
 	}
 
-	public static final void decrypt(byte[] key, byte[] data) {
+	public static final byte[] decrypt(byte[] key, byte[] data) {
 		/*
 		 * We do not allow for the key or the data to be null
 		 */
@@ -86,6 +67,7 @@ public final class AES {
 			throw new IllegalArgumentException("Key must be 16, 24, or 32 bytes long.");
 		}
 
+		return InvCypher.dec(key, data);
 	}
 
 	public static final ByteBuffer keyExpansion(byte[] key, int size, int nK) {
@@ -96,7 +78,7 @@ public final class AES {
 
 		for (int i = nK; i < size; i++) {
 			int temp = bufferInt.get(i - 1);
-			//System.out.printf("[ %d ]\t%8x", i, temp); // DEBUG
+			// System.out.printf("[ %d ]\t%8x", i, temp); // DEBUG
 			if ((i % nK) == 0) {
 				// This is the rotWord function
 				ByteBuffer work = ByteBuffer.allocate(4).putInt(temp);
@@ -107,31 +89,47 @@ public final class AES {
 				work.put(3, workTemp);
 				// End rotWord function
 				int rotWord = work.getInt(0);
-				// This is here so we can reuse another function and the work done for rotWord.
+				// This is here so we can reuse another function and the work
+				// done for rotWord.
 				Cypher.subBytes(work.array());
 				// End
 				int subWord = work.getInt(0);
 				int rcon = AES.rcon[(i / nK) - 1];
 				temp = subWord ^ rcon;
-				//System.out.printf("\t%8x\t%8x\t%8x\t%8x", rotWord, subWord, rcon, temp); // DEBUG
+				// System.out.printf("\t%8x\t%8x\t%8x\t%8x", rotWord, subWord,
+				// rcon, temp); // DEBUG
 			} else if ((nK > 6) && ((i % nK) == 4)) {
 				// This is only used for 256bit keys
 				ByteBuffer work = ByteBuffer.allocate(4).putInt(temp);
 				Cypher.subBytes(work.array());
 				int subWord = work.getInt(0);
 				temp = subWord;
-				//System.out.printf("\t%8x\t%8x\t%8x\t%8x", 0, subWord, 0, temp); // DEBUG
+				// System.out.printf("\t%8x\t%8x\t%8x\t%8x", 0, subWord, 0,
+				// temp); // DEBUG
 			} else {
-				//System.out.printf("\t%8x\t%8x\t%8x\t%8x", 0, 0, 0, 0); // DEBUG (this makes it look nice)
+				// System.out.printf("\t%8x\t%8x\t%8x\t%8x", 0, 0, 0, 0); //
+				// DEBUG (this makes it look nice)
 			}
 
 			int wink = bufferInt.get(i - nK);
 			int w = temp ^ wink;
 			bufferInt.put(i, w);
-			//System.out.printf("\t%8x\t%8x\n", wink, w); // DEBUG
+			// System.out.printf("\t%8x\t%8x\n", wink, w); // DEBUG
 		}
 
 		return buffer;
+	}
+
+	private static final byte[] addRoundKey(byte[] state, ByteBuffer w, int round) {
+
+		ByteBuffer work = ByteBuffer.wrap(state);
+		
+		for (int i = 0; i < 4; i++) {
+			work.putInt(work.getInt(i * 4) ^ w.getInt(round * 16 + (i * 4)));
+		}
+		System.out.println();
+
+		return state;
 	}
 
 	private static final class Cypher {
@@ -174,29 +172,30 @@ public final class AES {
 
 			ByteBuffer w = keyExpansion(key, (nB * (nR + 1)), nK);
 
-			
 			state = addRoundKey(state, w, 0);
 
-			//System.out.println("[ 1 ] "+AES.bytesToHex(state));
+			// System.out.println("[ 1 ] "+AES.bytesToHex(state));
 			for (int round = 1; round <= nR - 1; round++) {
-				
+
 				state = subBytes(state);
 				state = shiftRows(state);
 				state = mixColumns(state);
 				state = addRoundKey(state, w, round);
-				//System.out.println("[ " + (round + 1) +" ] "+AES.bytesToHex(state));
+				// System.out.println("[ " + (round + 1) +" ]
+				// "+AES.bytesToHex(state));
 			}
-			
+
 			state = subBytes(state);
 			state = shiftRows(state);
 			state = addRoundKey(state, w, nR);
-			//System.out.println(AES.bytesToHex(state));
+			// System.out.println(AES.bytesToHex(state));
 			return state;
 		}
 
 		/**
 		 * Implementation of section 5.1.1 using a pre-calculated s-box with
-		 * affine transformation already applied and placed into a 1d array for easy access.
+		 * affine transformation already applied and placed into a 1d array for
+		 * easy access.
 		 * 
 		 * @param state
 		 * @return
@@ -206,7 +205,7 @@ public final class AES {
 			for (int i = 0; i < state.length; i++) {
 				state[i] = (byte) sBox[state[i] & 0xFF];
 			}
-			
+
 			return state;
 		}
 
@@ -227,7 +226,7 @@ public final class AES {
 					// NOTE: This could also be hardcoded to properly shift all
 					// values instead of a loop
 					byte temp = state[i]; // get the first element in
-													// a row
+											// a row
 					// Shift the matrix row
 					state[i + 0] = state[i + 4];
 					state[i + 4] = state[i + 8];
@@ -238,64 +237,198 @@ public final class AES {
 
 			return state;
 		}
-		
+
 		/**
-		 * Helper method to do multiplication because for some reason it needs to be complicated
+		 * Helper method to do multiplication because for some reason it needs
+		 * to be complicated
 		 * 
 		 * @param a
 		 * @param b
 		 * @return
 		 */
-		private static final byte GMul(byte a, byte b) { // Galois Field (256) Multiplication of two Bytes
-			   byte p = 0;
-			   byte counter;
-			   byte hi_bit_set;
-			   for (counter = 0; counter < 8; counter++) {
-			      if ((b & 1) != 0) {
-			         p ^= a;
-			      }
-			      hi_bit_set = (byte) (a & 0x80);
-			      a <<= 1;
-			      if (hi_bit_set != 0) {
-			         a ^= 0x1b; /* x^8 + x^4 + x^3 + x + 1 */
-			      }
-			      b >>= 1;
-			   }
-			   return p;
+		private static final byte GMul(byte a, byte b) { // Galois Field (256)
+															// Multiplication of
+															// two Bytes
+			byte p = 0;
+			byte counter;
+			byte hi_bit_set;
+			for (counter = 0; counter < 8; counter++) {
+				if ((b & 1) != 0) {
+					p ^= a;
+				}
+				hi_bit_set = (byte) (a & 0x80);
+				a <<= 1;
+				if (hi_bit_set != 0) {
+					a ^= 0x1b; /* x^8 + x^4 + x^3 + x + 1 */
+				}
+				b >>= 1;
 			}
+			return p;
+		}
 
 		/**
 		 * implementation of section 5.1.3
 		 */
 		private static final byte[] mixColumns(byte[] state) {
-			
-			for(int i = 0; i < 4; i++) {
+
+			for (int i = 0; i < 4; i++) {
 				byte s0 = state[0 + (i * 4)];
-	byte s1 = state[1 + (i * 4)];
+				byte s1 = state[1 + (i * 4)];
 				byte s2 = state[2 + (i * 4)];
 				byte s3 = state[3 + (i * 4)];
-				
+
 				state[0 + (i * 4)] = (byte) (GMul((byte) (2), s0) ^ GMul((byte) (3), s1) ^ s2 ^ s3);
 				state[1 + (i * 4)] = (byte) (s0 ^ GMul((byte) (2), s1) ^ GMul((byte) (3), s2) ^ s3);
 				state[2 + (i * 4)] = (byte) (s0 ^ s1 ^ GMul((byte) (2), s2) ^ GMul((byte) (3), s3));
 				state[3 + (i * 4)] = (byte) (GMul((byte) (3), s0) ^ s1 ^ s2 ^ GMul((byte) (2), s3));
 			}
-			
-			return state;
-		}
 
-		private static final byte[] addRoundKey(byte[] state, ByteBuffer w, int round) {
-
-			ByteBuffer work = ByteBuffer.wrap(state);
-			for(int i = 0; i < 4; i++) {
-				work.putInt(work.getInt(i * 4) ^ w.getInt(round * 16 + (i * 4)));
-			}
-			
 			return state;
 		}
 	}
 
 	private static final class InvCypher {
 
+		private static final char[] invSBox = { 0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E,
+				0x81, 0xF3, 0xD7, 0xFB, 0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4,
+				0xDE, 0xE9, 0xCB, 0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA,
+				0xC3, 0x4E, 0x08, 0x2E, 0xA1, 0x66, 0x28, 0xD9, 0x24, 0xB2, 0x76, 0x5B, 0xA2, 0x49, 0x6D, 0x8B, 0xD1,
+				0x25, 0x72, 0xF8, 0xF6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xD4, 0xA4, 0x5C, 0xCC, 0x5D, 0x65, 0xB6, 0x92,
+				0x6C, 0x70, 0x48, 0x50, 0xFD, 0xED, 0xB9, 0xDA, 0x5E, 0x15, 0x46, 0x57, 0xA7, 0x8D, 0x9D, 0x84, 0x90,
+				0xD8, 0xAB, 0x00, 0x8C, 0xBC, 0xD3, 0x0A, 0xF7, 0xE4, 0x58, 0x05, 0xB8, 0xB3, 0x45, 0x06, 0xD0, 0x2C,
+				0x1E, 0x8F, 0xCA, 0x3F, 0x0F, 0x02, 0xC1, 0xAF, 0xBD, 0x03, 0x01, 0x13, 0x8A, 0x6B, 0x3A, 0x91, 0x11,
+				0x41, 0x4F, 0x67, 0xDC, 0xEA, 0x97, 0xF2, 0xCF, 0xCE, 0xF0, 0xB4, 0xE6, 0x73, 0x96, 0xAC, 0x74, 0x22,
+				0xE7, 0xAD, 0x35, 0x85, 0xE2, 0xF9, 0x37, 0xE8, 0x1C, 0x75, 0xDF, 0x6E, 0x47, 0xF1, 0x1A, 0x71, 0x1D,
+				0x29, 0xC5, 0x89, 0x6F, 0xB7, 0x62, 0x0E, 0xAA, 0x18, 0xBE, 0x1B, 0xFC, 0x56, 0x3E, 0x4B, 0xC6, 0xD2,
+				0x79, 0x20, 0x9A, 0xDB, 0xC0, 0xFE, 0x78, 0xCD, 0x5A, 0xF4, 0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7,
+				0x31, 0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F, 0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D,
+				0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF, 0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0, 0xC8,
+				0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61, 0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69,
+				0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D };
+
+		private static final byte[] dec(byte[] key, byte[] state) {
+			// number of words in the key
+			int nK = (key.length * 8) / 32;
+			// number of words per block
+			int nB = 4; // this is always 4 as the block size is constant in AES
+						// though it could be different
+			// number of rounds to do
+			int nR = nK + 6; // random equation that is currently always right
+
+			ByteBuffer w = keyExpansion(key, (nB * (nR + 1)), nK);
+			
+			//System.out.println("[ 0 ] "+AES.bytesToHex(state));
+			state = addRoundKey(state, w, nR);
+
+			//System.out.println("[ 1 ] "+AES.bytesToHex(state));
+			for (int round = nR - 1; round >= 1; round--) {
+
+				state = invShiftRows(state);
+				state = invSubBytes(state);
+				state = addRoundKey(state, w, round);
+				state = invMixColumns(state);
+				//System.out.println("[ " + (round + 1) +" ]"+AES.bytesToHex(state));
+			}
+
+			state = invShiftRows(state);
+			state = invSubBytes(state);
+			state = addRoundKey(state, w, 0);
+			//System.out.println("Output " + AES.bytesToHex(state));
+			return state;
+		}
+
+		/**
+		 * Implementation of section 5.3.2 using a pre-calculated s-box with
+		 * affine transformation already applied and placed into a 1d array for
+		 * easy access.
+		 * 
+		 * @param state
+		 * @return
+		 */
+		private static final byte[] invSubBytes(byte[] state) {
+
+			for (int i = 0; i < state.length; i++) {
+				state[i] = (byte) invSBox[state[i] & 0xFF];
+			}
+
+			return state;
+		}
+
+		/**
+		 * Implementation of section 5.1.2 using two for loops in place of
+		 * hardcoding the transformations.
+		 * 
+		 * @param state
+		 * @return
+		 */
+		private static final byte[] invShiftRows(byte[] state) {
+
+			// This is the current "row" we are working on. `i` is also the
+			// shift amount for each row
+			for (int i = 0; i < 4; i++) {
+				// actually run the shift.
+				for (int j = 0; j < i; j++) {
+					// NOTE: This could also be hardcoded to properly shift all
+					// values instead of a loop
+					byte temp = state[i + 12]; // get the last element in
+											// a row
+					// Shift the matrix row
+					state[i + 12] = state[i + 8];
+					state[i + 8] = state[i + 4];
+					state[i + 4] = state[i + 0];
+					state[i + 0] = temp;
+				}
+			}
+
+			return state;
+		}
+
+		/**
+		 * Helper method to do multiplication because for some reason it needs
+		 * to be complicated
+		 * 
+		 * @param a
+		 * @param b
+		 * @return
+		 */
+		private static final byte GMul(byte a, byte b) { // Galois Field (256)
+															// Multiplication of
+															// two Bytes
+			byte p = 0;
+			byte counter;
+			byte hi_bit_set;
+			for (counter = 0; counter < 8; counter++) {
+				if ((b & 1) != 0) {
+					p ^= a;
+				}
+				hi_bit_set = (byte) (a & 0x80);
+				a <<= 1;
+				if (hi_bit_set != 0) {
+					a ^= 0x1b; /* x^8 + x^4 + x^3 + x + 1 */
+				}
+				b >>= 1;
+			}
+			return p;
+		}
+
+		/**
+		 * implementation of section 5.1.3
+		 */
+		private static final byte[] invMixColumns(byte[] state) {
+
+			for (int i = 0; i < 4; i++) {
+				byte s0 = state[0 + (i * 4)];
+				byte s1 = state[1 + (i * 4)];
+				byte s2 = state[2 + (i * 4)];
+				byte s3 = state[3 + (i * 4)];
+				
+				state[0 + (i * 4)] = (byte) (GMul((byte) 0x0E, s0) ^ GMul((byte) 0x0B, s1) ^ GMul((byte) 0x0D, s2) ^ GMul((byte) 0x09, s3));
+				state[1 + (i * 4)] = (byte) (GMul((byte) 0x09, s0) ^ GMul((byte) 0x0E, s1) ^ GMul((byte) 0x0B, s2) ^ GMul((byte) 0x0D, s3));
+				state[2 + (i * 4)] = (byte) (GMul((byte) 0x0D, s0) ^ GMul((byte) 0x09, s1) ^ GMul((byte) 0x0E, s2) ^ GMul((byte) 0x0B, s3));
+				state[3 + (i * 4)] = (byte) (GMul((byte) 0x0B, s0) ^ GMul((byte) 0x0D, s1) ^ GMul((byte) 0x09, s2) ^ GMul((byte) 0x0E, s3));
+			}
+
+			return state;
+		}
 	}
 }
--- src/test/java/tests/AESTests.java
+++ src/test/java/tests/AESTests.java
@@ -75,19 +75,26 @@ public class AESTests {
 	public void encTest() {
 		byte[] key = { 0x2b, 0x7e, 0x15, 0x16, 0x28, (byte) 0xae, (byte) 0xd2, (byte) 0xa6, (byte) 0xab, (byte) 0xf7, 0x15, (byte) 0x88, 0x09, (byte) 0xcf, 0x4f, 0x3c };
 		byte[] data = { 0x32, 0x43, (byte) 0xf6, (byte) 0xa8, (byte) 0x88, 0x5a, 0x30, (byte) 0x8d, 0x31, 0x31, (byte) 0x98, (byte) 0xa2, (byte) 0xe0, 0x37, 0x07, 0x34 };
-		byte[] expecteds = { 0x39, 0x02, (byte) 0xdc, 0x19, 0x25, (byte) 0xdc, 0x11, 0x6a, (byte) 0x84, 0x09, (byte) 0x85, 0x0b, 0x1d, (byte) 0xfb, (byte) 0x97, 0x32 };
 		Assert.assertEquals("3925841D02DC09FBDC118597196A0B32", bytesToHex(AES.encrypt(key, data)));
 	}
 	
+	@Test
+	public void encdecTest() {
+		byte[] key = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f };
+		byte[] data = { 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, (byte) 0x88, (byte) 0x99, (byte) 0xaa, (byte) 0xbb, (byte) 0xcc, (byte) 0xdd, (byte) 0xee, (byte) 0xff };
+		String expecteds = bytesToHex(data);
+		Assert.assertEquals(expecteds, bytesToHex(AES.decrypt(key, AES.encrypt(key, data))));
+	}
+	
 	// useful for debugging
-		final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
-		public static String bytesToHex(byte[] bytes) {
-		    char[] hexChars = new char[bytes.length * 2];
-		    for ( int j = 0; j < bytes.length; j++ ) {
-		        int v = bytes[j] & 0xFF;
-		        hexChars[j * 2] = hexArray[v >>> 4];
-		        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-		    }
-		    return new String(hexChars);
-		}
+	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+	public static String bytesToHex(byte[] bytes) {
+	    char[] hexChars = new char[bytes.length * 2];
+	    for ( int j = 0; j < bytes.length; j++ ) {
+	        int v = bytes[j] & 0xFF;
+	        hexChars[j * 2] = hexArray[v >>> 4];
+	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+	    }
+	    return new String(hexChars);
+	}
 }
